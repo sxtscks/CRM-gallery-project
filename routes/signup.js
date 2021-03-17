3057de0c7922d381fbd4931c9838d38fd8924320
Kirill <cvj.sp.bk@gmail.com>
2021-03-17 19:51:06 +0300

    minor fixes

--- routes/signup.js
+++ routes/signup.js
@@ -1,7 +1,25 @@
 const router = require('express').Router();
+const Client = require('../models/clients')
 
 router.get('/', (req, res) => {
   res.render('signup');
 });
 
+router.post('/', async (req, res) => {
+  const { name, email, password: plainedPass } = req.body
+  if (name && email && plainedPass) {
+    const password = await bcrypt.hash(plainedPass, saltRound)
+    const newUser = await Client.create({
+      name, email, password
+    })
+    req.session.userId = newUser._id
+    req.session.email = newUser.email
+    req.session.name = newUser.name
+
+    return res.redirect('/entries')
+  }
+  return res.status(418).redirect('/register')
+})
+
 module.exports = router;
+
