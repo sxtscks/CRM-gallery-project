d28b1904d2c1fa5f52641779d38304013e9a46a0
Kirill <cvj.sp.bk@gmail.com>
2021-03-17 18:37:49 +0300

    minor fixes in database

--- models/pictures.js
+++ models/pictures.js
@@ -1,14 +1,22 @@
 const { model, Schema } = require('mongoose')
 
-const picturesSchema = new Schema ({
+const picturesSchema = new Schema({
   title: {
     type: String,
     required: true
-  }, 
+  },
   author: {
     type: String,
     required: true
-  }
+  },
+  cost: {
+    type: Number,
+    required: true
+  },
+  image: {
+    data: Buffer,
+    contentType: String
+  },
 })
 
 module.exports = model('pictures', picturesSchema)
