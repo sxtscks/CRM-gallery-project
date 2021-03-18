707d144be6252042839e103d4f59829e866c1de7
Kirill <cvj.sp.bk@gmail.com>
2021-03-18 16:33:07 +0300

    delete button added

--- public/js/application.js
+++ public/js/application.js
@@ -1,1 +1,18 @@
+const deleteButton = document.querySelector('[data-delete]')
 
+
+deleteButton.addEventListener('click', async (event) => {
+  event.preventDefault()
+  console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>', event.target);
+  const deleteId = event.target.dataset.delete
+  console.log(deleteId);
+  const response = await fetch(`/card/${deleteId}`, {
+    method: "DELETE"
+  })
+
+  if (response.status === 200) {
+    window.location.replace('/clients')
+  } else {
+    console.log('ERROR');
+  }
+})
--- routes/card.js
+++ routes/card.js
@@ -6,6 +6,10 @@ router.get('/:id', async (req, res) => {
   const client = await Client.findById(req.params.id);
   res.render('card', { client });
 });
-  
+
+router.delete('/:id', async (req, res, next) => {
+  await Client.findByIdAndDelete(req.params.id)
+  return res.sendStatus(200);
+})
 
 module.exports = router;
