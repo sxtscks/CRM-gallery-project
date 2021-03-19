e6cca612ae87bd297a6e62b3c64f0b62cb0ba7df
Ayuna <aunmiku@gmail.com>
2021-03-19 16:13:28 +0300

    save

--- public/js/application.js
+++ public/js/application.js
@@ -51,3 +51,30 @@ deleteButton.addEventListener('click', async (event) => {
   }
 })
 }
+
+
+const buttonSortCreate = document.querySelector('[data-sort-create]');
+
+// console.log(buttonSortCreate);
+if (buttonSortCreate) {
+  buttonSortCreate.addEventListener('click', async (e) => {
+    console.log('======Сортировка');
+  
+    const isSortCreate = true;
+  
+    const response = await fetch('/clients', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(isSortCreate),
+    });
+  
+    if (response.status !== 200) {
+      // errorDiv.innerText = 'Не удалось изменить данные клиента.';
+      return;
+    }
+  
+  })
+}
+
