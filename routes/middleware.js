6a2964c97b05d9b623956b03bb4c6daf64e425f9
Ayuna <aunmiku@gmail.com>
2021-03-19 10:20:22 +0300

    add return middleware

--- routes/middleware.js
+++ routes/middleware.js
@@ -2,7 +2,7 @@ function authenticated(req, res, next) {
   if (res.locals.userId) {
     return next();
   }
-  res.redirect('/login');
+  return res.redirect('/login');
 }
 
 module.exports = authenticated;
