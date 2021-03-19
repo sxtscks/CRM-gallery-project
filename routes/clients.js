7b5d96d790c376671bf9d680f36ffda52c90e360
Ayuna <aunmiku@gmail.com>
2021-03-19 15:55:26 +0300

    fetch sort

--- routes/clients.js
+++ routes/clients.js
@@ -4,6 +4,7 @@ const authenticated = require('./middleware');
 
 
 router.get('/', authenticated, async (req, res) => {
+
   try {
     const Clients = await Client.find();
     return res.render('clients', { Clients });
@@ -14,5 +15,26 @@ router.get('/', authenticated, async (req, res) => {
     })
   }
 });
+
+router.post('/', authenticated, async (req, res) => {
+  const { isSortCreate } = req.body;
+  console.log(isSortCreate);
+
+  try {
+    let Clients;
+    if (isSortCreate) {
+      console.log(isSortCreate);
+      Clients = await Client.find().sort({ createdAt: -1 }).exec();
+    } else {
+      Clients = await Client.find();
+    }
+    return res.render('clients', { Clients });
+    
+  } catch (error) {
+    return res.render('error', { 
+      message: 'Не удалось загрузить список клиентов.',
+    })
+  }
+})
 
 module.exports = router;
